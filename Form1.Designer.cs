ee52e9dedff80003e67c0261563194e5fe5387c5
Caio Eduardo <caiohernandezalmeida@gmail.com>
2025-04-01 15:58:47 -0300

    primeiro commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace ConnectPostgre
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.mkt_CPF = new System.Windows.Forms.MaskedTextBox();
+            this.mkt_DN = new System.Windows.Forms.MaskedTextBox();
+            this.Txt_Nome = new System.Windows.Forms.TextBox();
+            this.Txt_Email = new System.Windows.Forms.TextBox();
+            this.Txt_Telefone = new System.Windows.Forms.TextBox();
+            this.btn_Cancelar = new System.Windows.Forms.Button();
+            this.btn_Inserir = new System.Windows.Forms.Button();
+            this.btn_Consultar = new System.Windows.Forms.Button();
+            this.btn_Alterar = new System.Windows.Forms.Button();
+            this.btn_Excluir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CPF";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Telefone";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(35, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "E-mail";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(35, 186);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(179, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Data de Nascimento";
+            // 
+            // mkt_CPF
+            // 
+            this.mkt_CPF.Location = new System.Drawing.Point(156, 50);
+            this.mkt_CPF.Mask = "999.999.999-99";
+            this.mkt_CPF.Name = "mkt_CPF";
+            this.mkt_CPF.Size = new System.Drawing.Size(176, 20);
+            this.mkt_CPF.TabIndex = 5;
+            // 
+            // mkt_DN
+            // 
+            this.mkt_DN.Location = new System.Drawing.Point(220, 191);
+            this.mkt_DN.Mask = "00/00/0000";
+            this.mkt_DN.Name = "mkt_DN";
+            this.mkt_DN.Size = new System.Drawing.Size(83, 20);
+            this.mkt_DN.TabIndex = 6;
+            this.mkt_DN.ValidatingType = typeof(System.DateTime);
+            // 
+            // Txt_Nome
+            // 
+            this.Txt_Nome.Location = new System.Drawing.Point(156, 86);
+            this.Txt_Nome.MaxLength = 50;
+            this.Txt_Nome.Name = "Txt_Nome";
+            this.Txt_Nome.Size = new System.Drawing.Size(316, 20);
+            this.Txt_Nome.TabIndex = 7;
+            // 
+            // Txt_Email
+            // 
+            this.Txt_Email.Location = new System.Drawing.Point(156, 156);
+            this.Txt_Email.MaxLength = 100;
+            this.Txt_Email.Name = "Txt_Email";
+            this.Txt_Email.Size = new System.Drawing.Size(316, 20);
+            this.Txt_Email.TabIndex = 8;
+            // 
+            // Txt_Telefone
+            // 
+            this.Txt_Telefone.Location = new System.Drawing.Point(156, 120);
+            this.Txt_Telefone.MaxLength = 20;
+            this.Txt_Telefone.Name = "Txt_Telefone";
+            this.Txt_Telefone.Size = new System.Drawing.Size(316, 20);
+            this.Txt_Telefone.TabIndex = 9;
+            // 
+            // btn_Cancelar
+            // 
+            this.btn_Cancelar.Location = new System.Drawing.Point(352, 221);
+            this.btn_Cancelar.Name = "btn_Cancelar";
+            this.btn_Cancelar.Size = new System.Drawing.Size(120, 24);
+            this.btn_Cancelar.TabIndex = 10;
+            this.btn_Cancelar.Text = "Cancelar";
+            this.btn_Cancelar.UseVisualStyleBackColor = true;
+            this.btn_Cancelar.Click += new System.EventHandler(this.btn_Cancelar_Click);
+            // 
+            // btn_Inserir
+            // 
+            this.btn_Inserir.Location = new System.Drawing.Point(511, 35);
+            this.btn_Inserir.Name = "btn_Inserir";
+            this.btn_Inserir.Size = new System.Drawing.Size(104, 48);
+            this.btn_Inserir.TabIndex = 11;
+            this.btn_Inserir.Text = "Inserir";
+            this.btn_Inserir.UseVisualStyleBackColor = true;
+            this.btn_Inserir.Click += new System.EventHandler(this.btn_Inserir_Click);
+            // 
+            // btn_Consultar
+            // 
+            this.btn_Consultar.Location = new System.Drawing.Point(511, 89);
+            this.btn_Consultar.Name = "btn_Consultar";
+            this.btn_Consultar.Size = new System.Drawing.Size(104, 48);
+            this.btn_Consultar.TabIndex = 12;
+            this.btn_Consultar.Text = "Consultar";
+            this.btn_Consultar.UseVisualStyleBackColor = true;
+            this.btn_Consultar.Click += new System.EventHandler(this.btn_Consultar_Click);
+            // 
+            // btn_Alterar
+            // 
+            this.btn_Alterar.Location = new System.Drawing.Point(511, 143);
+            this.btn_Alterar.Name = "btn_Alterar";
+            this.btn_Alterar.Size = new System.Drawing.Size(104, 48);
+            this.btn_Alterar.TabIndex = 13;
+            this.btn_Alterar.Text = "Alterar";
+            this.btn_Alterar.UseVisualStyleBackColor = true;
+            this.btn_Alterar.Click += new System.EventHandler(this.btn_Alterar_Click);
+            // 
+            // btn_Excluir
+            // 
+            this.btn_Excluir.Location = new System.Drawing.Point(511, 197);
+            this.btn_Excluir.Name = "btn_Excluir";
+            this.btn_Excluir.Size = new System.Drawing.Size(104, 48);
+            this.btn_Excluir.TabIndex = 14;
+            this.btn_Excluir.Text = "Excluir";
+            this.btn_Excluir.UseVisualStyleBackColor = true;
+            this.btn_Excluir.Click += new System.EventHandler(this.btn_Excluir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 262);
+            this.Controls.Add(this.btn_Excluir);
+            this.Controls.Add(this.btn_Alterar);
+            this.Controls.Add(this.btn_Consultar);
+            this.Controls.Add(this.btn_Inserir);
+            this.Controls.Add(this.btn_Cancelar);
+            this.Controls.Add(this.Txt_Telefone);
+            this.Controls.Add(this.Txt_Email);
+            this.Controls.Add(this.Txt_Nome);
+            this.Controls.Add(this.mkt_DN);
+            this.Controls.Add(this.mkt_CPF);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Cadastro de Clientes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.MaskedTextBox mkt_CPF;
+        private System.Windows.Forms.MaskedTextBox mkt_DN;
+        private System.Windows.Forms.TextBox Txt_Nome;
+        private System.Windows.Forms.TextBox Txt_Email;
+        private System.Windows.Forms.TextBox Txt_Telefone;
+        private System.Windows.Forms.Button btn_Cancelar;
+        private System.Windows.Forms.Button btn_Inserir;
+        private System.Windows.Forms.Button btn_Consultar;
+        private System.Windows.Forms.Button btn_Alterar;
+        private System.Windows.Forms.Button btn_Excluir;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,382 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+
+// Npgsql é um ADO.NET Data Provider de código aberto para PostgreSQL
+// Ele permite que programas escritos em C#, Visual Basic, F#
+// Acessem o servidor de banco de dados PostgreSQL.
+// Ele é implementado em código 100% C#, é gratuito e de código aberto.
+using Npgsql;
+using NpgsqlTypes;
+using static System.Windows.Forms.LinkLabel;
+
+namespace ConnectPostgre
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Declarar Classe de Conexão e suas variáveis públicas
+        public NpgsqlConnection Conectar;
+        public NpgsqlCommand ComandoSQL;
+        public NpgsqlDataReader LerRegistro;
+        public string strCon =
+            "Server =localhost;Port=5432;Username=postgres;password=1243;Database=bancox";
+        public string strSQL = "";
+
+        // Não esquecer de programar o evento "Load" do Form1
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Desabilitando botões alterar e excluir (Ao abrir o Form1)
+            btn_Alterar.Enabled = false;
+            btn_Excluir.Enabled = false;
+            btn_Cancelar.Visible = false;
+            mkt_CPF.Focus();
+        }
+
+        // Programação dos eventos click de cada botão presente no Form1
+        private void btn_Inserir_Click(object sender, EventArgs e)
+        {
+            // Inserir Registro
+
+            // Bloco try/catch serve para tratamento de exceções (possíveis falhas ou erros),
+
+            // Tratamento de códigos que podem não ser totalmente atendidos e gerarem alguma exceção/erro.
+            try
+            {
+                // Lembramos que estamos usando o Objeto para CPF (MaskTextBox),
+                // Quando usamos a propriedade Mask, invertemos o simbolo (,) por (.) e vice-versa
+                // Analisando a consistência (validação) do Objeto "mkt_CPF",
+                // Onde não pode ser menor que 14 caracteres (extraindo os espaços vazios)
+                if (mkt_CPF.Text.Trim().Length == 14)
+                {
+                    // Criar e Estanciar Objeto a Classe de Conexão com o Danco de Dados
+                    Conectar = new NpgsqlConnection(strCon);
+                    // Abrir Objeto de Conexão com Banco de Dados criada acima;
+                    Conectar.Open();
+                    // Montando a variavel tipo String "strSQL" de Leitura de Dados(concatenando) com os objetos do Formulário
+                    strSQL = "SELECT * FROM clientes WHERE CPF = '" + mkt_CPF.Text + "'";
+
+                    // MessageBox.Show(message, title, buttons)
+                    // Mensagem para apresentar a String (strSQL)
+                    MessageBox.Show(strSQL);
+                    // Instanciando o Objeto de classe de Command (comando) para armazenar a Instrução / Clausulas SQL
+                    ComandoSQL = new NpgsqlCommand(strSQL, Conectar);
+                    // Executando comando com resposta de Leitura de Registros(linha por Linha)
+                    LerRegistro = ComandoSQL.ExecuteReader();
+                    // Metodo Read(): Informe um "boolean" "True"(Existe Registro) e "False"(Não Existe Registro),
+                    // Possibilita Ler o Proximo Registro de uma Tabela (Enquanto for True, Se existir registro)
+                    if (LerRegistro.Read())
+                    {
+                        MessageBox.Show("Cliente já¡ Existe!!!",
+                        "Sistema Informa");
+                    }
+                    else
+                    {
+                        // Fechar Classe DataReader e Dispose (limpar o Objeto) da Classe de ComandoSQL
+                        LerRegistro.Close();
+                        ComandoSQL.Dispose();
+                        ComandoSQL.Transaction = null;
+                        // Podemos efetuar a validação de todos os campos, antes de inserirmos o registro...
+                        // Montando a variavel tipo String "strSQL" de Inserção dos Dados(concatenando) com os objetos do Formulário
+                        strSQL = "INSERT INTO clientes " +
+                        "(cpf, nome, telefone, email, dn) " +
+                        "VALUES (" +
+                        "'" + mkt_CPF.Text + "'," +
+                        "'" + Txt_Nome.Text + "'," +
+                        "'" + Txt_Telefone.Text + "'," +
+                        "'" + Txt_Email.Text + "'," +
+                        "TO_DATE ('" + mkt_DN.Text + "', 'DD/ MM / YYYY'));";
+                        // Mensagem para apresentar a String (strSQL)
+                        MessageBox.Show(strSQL);
+                        // Criar o Objeto com a classe de Command (comando) para armazenar a Instrução / Comando SQL
+                        // comandoSQL já etá definida como uma Classe NpgsqlCommand, portanto só precisamos Instaciá-la¡
+                        ComandoSQL = new NpgsqlCommand(strSQL, Conectar);
+                        // Executando sem resposta
+                        ComandoSQL.ExecuteNonQuery();
+                        MessageBox.Show("Registro Inserido com Sucesso...", "Sistema Informa");
+                        // Limpar Objetos usando a função
+                        LimparObjetos();
+                    }
+
+                    // Fechar Conexão
+                    Conectar.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Preencher corretamente pelo menos o campo CNPJ!!!");
+                }
+
+                // Voltar cursor para o objeto de formulário
+                mkt_CPF.Focus();
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.Message);
+            }
+
+        }
+
+        private void btn_Consultar_Click(object sender, EventArgs e)
+        {
+            // Bloco try/catch serve para tratamento de exceções (possiveis falhas ou erros),
+            // Tratamento de códigos que podem não ser totalmente atendidos e gerarem alguma excessão / erro.
+
+            try
+            {
+                if (mkt_CPF.Text.Trim().Length == 14)
+                {
+                    // Instanciar Objeto da Classe de Conexão com o Banco de Dados
+                    Conectar = new NpgsqlConnection(strCon);
+
+                    // Abrir Objeto de Conexão com Banco de Dados criada acima;
+                    Conectar.Open();
+
+                    // Montando a String (concatenando) com os objetos do Formulário
+                    strSQL = "SELECT * FROM Clientes WHERE (CPF =" + "'" + mkt_CPF.Text + "')";
+
+                    // Mensagem para apresentar a String (strSQL)
+                    MessageBox.Show(strSQL);
+
+                    // Instanciando o Objeto da classe de Command (comando) para armazenar a Instrução / Clausulas SQL
+                    ComandoSQL = new NpgsqlCommand(strSQL, Conectar);
+
+                    // OleDbDataReader rs = new OleDbDataReader (comandoSQL);
+                    // Executando sem resposta
+                    // ComandoSQL.ExecuteNonQuery();
+                    LerRegistro = ComandoSQL.ExecuteReader();
+
+                    // Metodo Read(): Informe um "boolean" "True" (existe Registro) e "False" (Não Existe Registro),
+                    // Possibilita Ler o Proximo Registro de uma Tabela (Enquanto for True, Se existir registro)
+
+                    if (LerRegistro.Read())
+                    {
+                        // Populando Objetos do Form com Dados do Registro(lerRegistro)
+                        mkt_CPF.Text = LerRegistro.GetString(0);
+                        Txt_Nome.Text = LerRegistro.GetString(1);
+                        Txt_Telefone.Text = LerRegistro.GetString(2);
+                        Txt_Email.Text = LerRegistro.GetString(3);
+                        mkt_DN.Text = LerRegistro.GetDateTime
+                        (4).ToString("dd/MM/yyyy");
+
+                        // Opção de Edição
+                        if (DialogResult.No == MessageBox.Show("Deseja Editar Registro ? ", "Sistema Informa", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2))
+                        {
+                            // Limpar Objetos do Formulário usando a função
+                            LimparObjetos();
+
+                            // Destaiva Botões
+                            btn_Alterar.Enabled = false;
+                            btn_Excluir.Enabled = false;
+
+                            // Ativa CPF (Edição)
+                            mkt_CPF.Enabled = true;
+                        }
+                        else
+                        {
+                            // Ativa Botões
+                            btn_Alterar.Enabled = true;
+                            btn_Excluir.Enabled = true;
+
+                            // Desativa Botões
+                            btn_Inserir.Enabled = false;
+                            btn_Consultar.Enabled = false;
+
+                            // Desativa CPF (Edição)
+                            mkt_CPF.Enabled = false;
+
+                            // Visualiza
+                            btn_Cancelar.Visible = true;
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("CPF " + mkt_CPF.Text + " Não Localizado!!!", "Sistema Informa");
+                        mkt_CPF.Focus();
+                    }
+
+                    // Fechar Classe DataReader e Dispose (limpar o Objeto) da Classe de ComandoSQL
+                    LerRegistro.Close();
+                    ComandoSQL.Dispose();
+                    ComandoSQL.Transaction = null;
+
+                    // Fechar Conexão
+                    Conectar.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Preencher corretamente pelo menos o campo CPF!!!");
+                }
+
+                // Votar Cursor para o Objeto de Formulario
+                mkt_CPF.Focus();
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.Message);
+            }
+        }
+
+        private void btn_Alterar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (DialogResult.Yes == MessageBox.Show("Confirma alteração do registro?", "Sistema Informa", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2))
+                {
+                    // Instanciar objeto da classe de conexão com o banco de dados
+                    Conectar = new NpgsqlConnection(strCon);
+
+                    // Abrir objeto de conexão com banco de dados criada acima;
+                    Conectar.Open();
+
+                    // Montando a string (concatenando) com os objetos do formulário
+                    strSQL = "UPDATE Clientes SET " + "nome = '" + Txt_Nome.Text + "', " + "telefone = '" + Txt_Telefone.Text + "' " + "email = '" + Txt_Email.Text + "' " + "dn = TO_DATE ('" + mkt_DN.Text + "', 'DD/MM/YYYY') " + " WHERE cpf = '" + mkt_CPF.Text + "' ";
+
+                    // Mensagem para apresentar a String (strSQL)
+                    MessageBox.Show(strSQL);
+
+                    // Instanciando o objeto da classe de Command (comando) para armazenar a Instrução / Clausulas SQL
+                    ComandoSQL = new NpgsqlCommand(strSQL, Conectar);
+
+                    // Executando sem resposta
+                    ComandoSQL.ExecuteNonQuery();
+                    MessageBox.Show("Registro Alterado com Sucesso...", "Sistema Informa");
+
+                    // Limpar Objetos do Formulário usando a função
+                    LimparObjetos();
+                    // Destaiva Botões
+                    btn_Alterar.Enabled = false;
+                    btn_Excluir.Enabled = false;
+                    // Ativa Botões
+                    btn_Consultar.Enabled = true;
+                    btn_Inserir.Enabled = true;
+                    // Esconte / Não Visusaliza
+                    btn_Cancelar.Visible = false;
+                    // Ativa CPF (Edição)
+                    mkt_CPF.Enabled = true;
+                    // Volta cursor ao objeto CPF
+                    mkt_CPF.Focus();
+                    // Fechar Conexão
+                    Conectar.Close();
+                }
+                else
+                {
+                    // Volta para edição
+                    Txt_Nome.Focus();
+                }
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.Message);
+            }
+        }
+
+        private void btn_Excluir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (DialogResult.Yes == MessageBox.Show("Confirma Exclusão do Registro ? ", "Sistema Informa", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2))
+                {
+                    // Instanciar Objeto da Classe de ConexÃ£o com o banco de Dados
+                    Conectar = new NpgsqlConnection(strCon);
+
+                    // Abrir Objeto de ConexÃ£o com Banco de Dados criada acima;
+                    Conectar.Open();
+
+                    // Montando a String (concatenando) com os objetos do Formulário
+                    strSQL = "DELETE FROM Clientes WHERE cpf = '" + mkt_CPF.Text + "' ";
+
+                    // Mensagem para apresentar a String (strSQL)
+                    MessageBox.Show(strSQL);
+
+                    // Instanciando o Objeto da classe de Command (comando) para armazenar a Instrução / Clausulas SQL
+                    ComandoSQL = new NpgsqlCommand(strSQL, Conectar);
+
+                    // Executando sem resposta
+                    ComandoSQL.ExecuteNonQuery();
+                    MessageBox.Show("Registro Excluído com Sucesso...", "Sistema Informa");
+                }
+                else
+                {
+                    MessageBox.Show("Operação Cancelada...", "Sistema Informa");
+                }
+
+                // Limpar Objetos do Formulário usando a função
+                LimparObjetos();
+
+                // Desativando Botões
+                btn_Alterar.Enabled = false;
+                btn_Excluir.Enabled = false;
+
+                // Ativando Botões
+                btn_Consultar.Enabled = true;
+                btn_Inserir.Enabled = true;
+
+                // Esconde / Não Visusaliza
+                btn_Cancelar.Visible = false;
+
+                // Ativa CPF (Edição)
+                mkt_CPF.Enabled = true;
+
+                // Volta cursor ao objeto CPF
+                mkt_CPF.Focus();
+
+                // Fechar Conexão
+                Conectar.Close();
+            }
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.Message);
+            }
+        }
+
+        private void btn_Cancelar_Click(object sender, EventArgs e)
+        {
+            // Limpar Objetos do Formulário usando a função
+            LimparObjetos();
+
+            // Desativa Botões
+            btn_Alterar.Enabled = false;
+            btn_Excluir.Enabled = false;
+
+            // Ativa Botões
+            btn_Inserir.Enabled = true;
+            btn_Consultar.Enabled = true;
+
+            // Ativa CPF (Edição)
+            mkt_CPF.Enabled = true;
+
+            // Volta cursor ao objeto CPF
+            mkt_CPF.Focus();
+
+            // Fechar Conexão
+            Conectar.Close();
+            btn_Cancelar.Visible = false;
+        }
+
+        // Criando Função LimparObjetos
+        public void LimparObjetos()
+        {
+            // Limpar Objetos do Formulário
+            mkt_CPF.Clear();
+            Txt_Nome.Clear();
+            Txt_Telefone.Clear();
+            Txt_Email.Clear();
+            mkt_DN.Clear();
+            return;
+
+        }
+    }
+}
